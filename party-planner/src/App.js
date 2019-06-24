2b2611e0db740c5f8f711959af11fa176bb341ec
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 10:21:26 -0700

    Parties component import in App.js

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Parties from './views/parties';
 
 function App() {
   return (
     <div className="App">
-
+      <Parties />
     </div>
   );
 }
