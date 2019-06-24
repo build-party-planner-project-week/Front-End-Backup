ceb667c7aeae44de65e210c4f64cdd7474166a0e
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 15:29:32 -0700

    PrivateRoute redirect if token is not in localStorage

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './utility/PrivateRoute'
 import Parties from "./views/Parties";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <PrivateRoute exact path="/parties" component={Parties} />
         <Parties />
       </div>
     </Router>
