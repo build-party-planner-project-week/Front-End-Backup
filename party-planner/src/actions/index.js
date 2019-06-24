6edb6415dd971f882546702b4e27ab258070ff7c
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 10:17:55 -0700

    Finite state pattern for todo list

--- party-planner/src/actions/index.js
+++ party-planner/src/actions/index.js
@@ -1,6 +1,10 @@
 //TodoList
-export const ADD_TODO = "ADD_TODO"
+export const ADD_TODO_START = "ADD_TODO"
+export const ADD_TODO_SUCCESS = "ADD_TODO_SUCCESS"
+export const ADD_TODO_FAILED = "ADD_TODO_FAILED"
 
-export const addTodo = todo => {
-    
+
+export const addTodo = todo => dispatch => {
+    dispatch({type: ADD_TODO_START})
+    //make axios request and dispatch other actions based on resolved or rejected
 }
