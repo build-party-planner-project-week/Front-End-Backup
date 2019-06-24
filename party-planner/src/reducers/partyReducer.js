b487cd6e7ef811f412547b01f8e15d33d598e42b
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 10:10:21 -0700

    Party reducer scaffold create

--- party-planner/src/reducers/partyReducer.js
+++ party-planner/src/reducers/partyReducer.js
@@ -1,8 +1,15 @@
 const initialState = {
-    theme: '',
-    guests: '',
-    budget: 100,
-    date: '',
-    todoList: [],
-    shoppingList: []
-}
+  theme: "",
+  guests: "",
+  budget: 100,
+  date: "",
+  todoList: [],
+  shoppingList: []
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
