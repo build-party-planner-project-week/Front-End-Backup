3aaa39e2d7342dae7935cb74c7c596edb3179cd8
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 10:36:45 -0700

    Store create and export in store.js

--- party-planner/src/store.js
+++ party-planner/src/store.js
@@ -3,3 +3,6 @@ import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+export default store;
