f8492e3be6ef4ffd86d2519c70ac30bca4b69c02
Michael Harris <mhguitar1997@gmail.com>
2019-06-24 14:40:49 -0700

    Import toggleTodo action in Party.js

--- party-planner/src/views/Party.js
+++ party-planner/src/views/Party.js
@@ -1,8 +1,7 @@
 import React from "react";
 import TodoList from "./TodoList";
-import {connect} from 'react-redux'
-import {addTodo} from '../actions'
-
+import { connect } from "react-redux";
+import { addTodo, toggleTodo } from "../actions";
 
 //will connect to redux stare and pass info through props
 class Party extends React.Component {
@@ -10,26 +9,29 @@ class Party extends React.Component {
   //Render Todo list
   //render shopping list
 
-  addTodoItem  = todoItem => {
-    this.props.addTodo(todoItem)
-  }
+  addTodoItem = todoItem => {
+    this.props.addTodo(todoItem);
+  };
   toggleTodoItem = todoId => {
-      this.props.toggleTodo(todoId)
-  }
+    this.props.toggleTodo(todoId);
+  };
   render() {
     return (
       <div>
         <h1>Party!</h1>
-        <TodoList todos={this.props.todos} addTodoItem={this.addTodoItem}/>
+        <TodoList todos={this.props.todos} addTodoItem={this.addTodoItem} />
       </div>
     );
   }
 }
 
-//each party probably will need its own id 
+//each party probably will need its own id
 const mapStateToProps = state => {
-    return {
-        todos: state.partyReducer.todoList
-    }
-}
-export default connect(mapStateToProps, {addTodo})(Party);
+  return {
+    todos: state.partyReducer.todoList
+  };
+};
+export default connect(
+  mapStateToProps,
+  { addTodo, toggleTodo }
+)(Party);
