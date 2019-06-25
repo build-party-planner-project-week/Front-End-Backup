2f97449a311b8e19ee90f880ff697f3c0363bee2
rushi444 <rushi44@gmail.com>
2019-06-25 09:50:45 -0700

    initial on this brance

--- party-planner/src/reducers/index.js
+++ party-planner/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 // import loginReducer from "./loginReducer";
 import partyReducer from "./partyReducer";
 import { itemReducer } from "./shoppingReducer";
-import { budgetReducer } from "./shoppingReducer";
+import { budgetReducer } from "./shoppingReducer"; 
 
 export default combineReducers({
 //   loginReducer,
