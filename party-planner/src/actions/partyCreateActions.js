d6b74930aa16174f35af56f74402ddb755a86fb2
Michael Harris <mhguitar1997@gmail.com>
2019-06-26 00:10:44 -0700

    Attempt axios post call

--- party-planner/src/actions/partyCreateActions.js
+++ party-planner/src/actions/partyCreateActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import {axiosWithAuth} from '../utility\/axiosWithAuth'
 
 export const CREATE_PARTY_START = "CREATE_PARTY";
 export const CREATE_PARTY_SUCCESS = "CREATE_PARTY_SUCCESS";
@@ -9,6 +9,9 @@ export const createParty = partyInfo => dispatch => {
   dispatch({ type: CREATE_PARTY_START });
   //when this is triggered possibly go to a form or modal pop out were user can put in party information
   //make axios post request to server to add that party to databasegit
+  axiosWithAuth().post('/party', partyInfo)
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
   dispatch({type: CREATE_PARTY_SUCCESS}) //if axios request is successful
   dispatch({type: CREATE_PARTY_FAILED}) //if error
 };
